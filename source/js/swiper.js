db109adb774cb250b8d9e51df39bef52da3a07a1
MarinaMich <mar7995@yandex.ru>
2025-02-07 16:43:36 +0300

    Редактирует пагинацию у слайдера

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -4,13 +4,26 @@ import {Navigation, Pagination, Keyboard} from 'swiper/modules';
 // слайдер блока hero
 
 const heroSlider = new Swiper('.show-hero', {
-  modules: [Navigation, Pagination, Keyboard],
+  modules: [Pagination, Keyboard],
   loop: true,
   grabCursor: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.show-hero__pagination',
     type: 'bullets',
     clickable: true,
+  },
+  on: {
+    // переопределяет место пагинации для каждого слайда
+    activeIndexChange: function () {
+      const index = this.activeIndex;
+      const currentSlide = this.slides[index];
+      const currentContent = currentSlide.querySelector('.show-hero__content');
+      const pagination = document.querySelector('.show-hero__pagination');
+
+      const paginationHorisontally = (this.el.getBoundingClientRect().width - 320) / 2 + 31;
+      pagination.style.bottom = getComputedStyle(currentContent).height;
+      pagination.style.left = `${paginationHorisontally}px`;
+    }
   },
 });
 
