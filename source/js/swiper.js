301c74beadbc748a4a7a73e6d41074f9d7ce069c
MarinaMich <mar7995@yandex.ru>
2025-02-09 20:12:17 +0300

    Редактирует место пагинации в desktop

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -20,6 +20,8 @@ const heroSlider = new Swiper('.show-hero', {
       const currentContent = currentSlide.querySelector('.show-hero__content');
       const control = currentContent.querySelector('.show-hero__control');
       const pagination = document.querySelector('.show-hero__pagination');
+      const slider = document.querySelector('.hero__slider');
+
       // значения свойств блока show-hero__content
       let bottomContent = window.getComputedStyle(currentContent).bottom;
       bottomContent = parseInt(bottomContent, 10);
@@ -27,12 +29,21 @@ const heroSlider = new Swiper('.show-hero', {
       paddingTopControl = parseInt(paddingTopControl, 10);
       let paddingLeftControl = window.getComputedStyle(control).paddingLeft;
       paddingLeftControl = parseInt(paddingLeftControl, 10);
+      if (document.documentElement.clientWidth >= 1440) {
+        const paddingLeft = (slider.getBoundingClientRect().width - 1240) / 2;
+        currentContent.style.left = `${paddingLeft}px`;
+      }
+
       // расчет положения пагинации
       const paginationHorisontally = (this.el.getBoundingClientRect().width - currentContent.getBoundingClientRect().width) / 2 + paddingLeftControl;
       const paginationVertically = currentContent.getBoundingClientRect().height + bottomContent - paddingTopControl * 2;
       // присваивание новых свойств
       pagination.style.bottom = `${paginationVertically}px`;
       pagination.style.left = `${paginationHorisontally}px`;
+      if (document.documentElement.clientWidth >= 1440) {
+        const paginationLeft = (this.el.getBoundingClientRect().width - 1240) / 2 + 20;
+        pagination.style.left = `${paginationLeft}px`;
+      }
     }
   },
 });
